2b147573369a04849ab152cc3a898cf51a955397
Teja <tkoppine@asu.edu>
2025-10-30 18:24:10 -0700

    Updates

---
+++ _next/static/chunks/98d261d683ae3f45.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/df8bddaeeab89e32.js",
+  "static/chunks/ee29201506ae94bc.js",
+  "static/chunks/turbopack-07b06d641394bf3f.js"
+])
---
+++ _next/static/eSB5KYhGEg3m4v-uBCrlj/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/98d261d683ae3f45.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/my-portfolio//_next/:path+",
+        "destination": "/my-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
